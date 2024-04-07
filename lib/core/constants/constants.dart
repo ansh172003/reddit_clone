417fb0fafdca2755e15f10dc13dc6093339cb3bb
Ansh Goyal <ansh172003@gmail.com>
2024-04-07 16:16:37 +0530

    added bottom nav bar

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,3 +1,6 @@
+import 'package:reddit_app/features/feed/screen/feed_screen.dart';
+import 'package:reddit_app/features/post/screen/post_screen.dart';
+
 class Constant {
   static String redditLogoPath = "assets/images/logo.png";
   static String redditEmotePath = "assets/images/loginEmote.png";
@@ -6,4 +9,6 @@ class Constant {
       'https://thumbs.dreamstime.com/b/abstract-stained-pattern-rectangle-background-blue-sky-over-fiery-red-orange-color-modern-painting-art-watercolor-effe-texture-123047399.jpg';
   static const avatarDefault =
       'https://external-preview.redd.it/5kh5OreeLd85QsqYO1Xz_4XSLYwZntfjqou-8fyBFoE.png?auto=webp&s=dbdabd04c399ce9c761ff899f5d38656d1de87c2';
+
+  static const tabWidgets = [FeedScreen(), AddPostScreen()];
 }
